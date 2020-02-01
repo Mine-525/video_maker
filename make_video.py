95315c879f3d4e3f7be715e3aa20bbfb67b2693d
Mine <mine.go.0525@gmail.com>
2020-02-01 15:43:43 +0900

    [update] adapt pictures from phone
    携帯などの連続写真に対応
    コマンドライン引数のオプションに-pを加えることでいい感じにfpsを調整する

--- make_video.py
+++ make_video.py
@@ -6,23 +6,36 @@ Argument str:
 """
 import sys
 import glob
-import re
 import cv2
 from natsort import natsorted
+from argparse import ArgumentParser
 
 
-def main(_argv):
-    path = "./" + str(_argv) + "/*"
+
+def main():
+    usage = 'Usage: python {}FILE [--phone] [--help]'\
+            .format(__file__)
+    argparser = ArgumentParser(usage=usage)
+    argparser.add_argument('dname', type=str, help='echo dname')
+    argparser.add_argument('-p', '--phone', action='store_true', help='pictures are taken by a phone or not?')
+
+    args = argparser.parse_args()
+
+    path = "./" + str(args.dname) + "/*"
     pic_list = glob.glob(path)
     pic_list = natsorted(pic_list)
 
     fourcc = cv2.VideoWriter_fourcc('m','p','4','v')
-    video = cv2.VideoWriter(str(_argv) + '.mp4', fourcc, 20.0, (480, 360))
+    if args.phone:
+        fps = 10.0
+    else:
+        fps = 20.0
+
+    video = cv2.VideoWriter(str(args.dname) + '.mp4', fourcc, fps, (480, 360))
 
     for i in pic_list:
         img = cv2.imread(i)
         video.write(img)
 
 if __name__ == "__main__":
-    _argv = sys.argv[1]
-    main(_argv)
+    main()
